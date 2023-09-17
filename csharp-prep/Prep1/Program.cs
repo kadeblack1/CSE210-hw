af9853ae138f600900f5e1fb9df57678ee6fc614
Kade <1999kadeblack@gmail.com>
2023-09-16 18:59:59 -0600

    did assignemt

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is yout last name?");
+        string last = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last}, {first}, {last}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! My name is Kade Black");
+        Console.WriteLine("This is c#");
+
     }
-}
+    }
